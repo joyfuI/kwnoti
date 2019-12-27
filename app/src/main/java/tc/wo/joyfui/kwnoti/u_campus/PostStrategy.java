09ea7ecdc66c52e59167dd2f9267152e2d1d4864
joyfuI <jong970105@gmail.com>
2019-12-27 21:44:56 +0900

    코틀린으로 재개발 시작
    코틀린 연습용

--- app/src/main/java/tc/wo/joyfui/kwnoti/u_campus/PostStrategy.java
+++
@@ -1,9 +0,0 @@
-package tc.wo.joyfui.kwnoti.u_campus;
-
-import org.jsoup.nodes.Document;
-
-public interface PostStrategy {
-	String getTitle(Document doc);
-	String getContent(Document doc);
-	String getServlet();
-}
